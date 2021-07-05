e07f0ab33e7fa3e598a74d065096f8cd39dd9281
tmangrove <86983135+tmangrove@users.noreply.github.com>
2021-07-05 14:26:46 -0500

    Create WiscoHello.java

---
+++ WiscoHello.java
@@ -0,0 +1,8 @@
+public class WiscHello {
+
+    public static void main(String[] args) {
+        // Prints a northern Wisconsin greeting to the terminal window.
+        System.out.println("Oh hey. Howz it goin?");
+    }
+
+}
